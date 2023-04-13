787a25174e7b813949fd2b1e7462ec0f7c53f4d6
mrodriguezzsx <mrodrigu@jaumebalmes.net>
2023-04-13 20:46:26 +0200

    Practica18 sin css

--- JaumeBalmes_Matias/JaumeBalmes_Matias/settings.py
+++ JaumeBalmes_Matias/JaumeBalmes_Matias/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Practica16.apps.Practica16Config',
+    'practica18.apps.Practica18Config',
 ]
 
 MIDDLEWARE = [
